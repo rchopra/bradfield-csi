13f8d580f9f6e3675235f60c0988dc37f39ee843
Rishi Chopra <rqchopra@gmail.com>
2021-02-01 19:31:43 -0600

    Implement simplest ls with opendir and readdir
    For now, I'm just going off the `man` pages for these functions. Not
    sure I'm in love with the ultra-terse style of K&R, with so much logic
    taking place inside the condition checks of loops, but for now I'm going
    for it as that's my only reference for "idiomatic".

--- prep/1_c/ls.c
+++ prep/1_c/ls.c
@@ -1,3 +1,27 @@
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+void list_contents(DIR *dirp) {
+  // TODO: Sort files like `ls` by default
+  // TODO: Implement the -a flag; it is on by default now
+  struct dirent *ent;
+  // TODO: Error checking when calling `readdir`
+  while((ent = readdir(dirp))) {
+    printf("%s\t", ent->d_name);
+  }
+  printf("\n");
+}
+
 int main(int argc, char *argv[]) {
+  // TODO: Read the directory from the command line
+  char *path = ".";
+  DIR *dirp;
+
+  dirp = opendir(path);
+  // TODO: Some error checking
+
+  list_contents(dirp);
+
   return 0;
 }
