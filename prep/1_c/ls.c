5a12416cf7d4eaada47dc115db17fe1046722fb0
Rishi Chopra <rqchopra@gmail.com>
2021-02-01 20:10:33 -0600

    Read a directory name from the command line args
    The approach taken here will not suffice for dealing with optional
    argument flags, but will serve for proving out the initial
    implementation.

--- prep/1_c/ls.c
+++ prep/1_c/ls.c
@@ -2,23 +2,24 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+#define DEFAULT_DIR "."
+
 void list_contents(DIR *dirp) {
   // TODO: Sort files like `ls` by default
   // TODO: Implement the -a flag; it is on by default now
   struct dirent *ent;
   // TODO: Error checking when calling `readdir`
   while((ent = readdir(dirp))) {
+    //TODO: `ls` uses some kind of padding, not tab characters
     printf("%s\t", ent->d_name);
   }
   printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-  // TODO: Read the directory from the command line
-  char *path = ".";
-  DIR *dirp;
-
-  dirp = opendir(path);
+  // TODO: This will have to change when introducing optional flags
+  char *path = (argc == 1) ? DEFAULT_DIR : argv[1];
+  DIR *dirp = opendir(path);
   // TODO: Some error checking
 
   list_contents(dirp);
