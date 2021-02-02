4929c9c7f00d2af296674f70d315cb5780e5dcd3
Rishi Chopra <rqchopra@gmail.com>
2021-02-01 20:30:53 -0600

    Handle bad directory input
    As much as possible, I'm trying to clone `ls`, so I took the same format
    of error, which seems to be using just the standar library's `strerror`
    function. Also returning 1 as that seems to be what `ls` is doing on my
    machine, no matter the error, just playing around with it.

--- prep/1_c/ls.c
+++ prep/1_c/ls.c
@@ -1,6 +1,8 @@
 #include <dirent.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 
 #define DEFAULT_DIR "."
 
@@ -9,6 +11,7 @@ void list_contents(DIR *dirp) {
   // TODO: Implement the -a flag; it is on by default now
   struct dirent *ent;
   // TODO: Error checking when calling `readdir`
+  // TODO: Handle when input is just a file
   while((ent = readdir(dirp))) {
     //TODO: `ls` uses some kind of padding, not tab characters
     printf("%s\t", ent->d_name);
@@ -20,7 +23,11 @@ int main(int argc, char *argv[]) {
   // TODO: This will have to change when introducing optional flags
   char *path = (argc == 1) ? DEFAULT_DIR : argv[1];
   DIR *dirp = opendir(path);
-  // TODO: Some error checking
+
+  if (dirp == NULL) {
+    printf("%s: %s: %s\n", argv[0], path, strerror(errno));
+    return 1;
+  }
 
   list_contents(dirp);
 
