cc4f18ee05bce8d40c0cbf70b4cf76a4e59c64c6
Rishi Chopra <rqchopra@gmail.com>
2021-02-02 23:15:43 -0600

    Use a struct to collect entries before printing
    Because we have to potentially sort the list of entries, printing
    one-by-one in the order given by `readdir` is not going to cut it.
    Instead, allocate an array of custom structs that will be a small subset
    of the `struct stat` returned by stat, only what's needed for printing.
    
    For now I've arbitrarily set the number of files in the directory to
    2^16 - 1, but it's probably worth revisting this decision. It would be
    nice to actually implement as a stream but I'm not sure how to sort
    that.

--- prep/1_c/ls.c
+++ prep/1_c/ls.c
@@ -7,32 +7,47 @@
 #include <sys/stat.h>
 
 #define DEFAULT_DIR "."
+#define MAX_ENTRIES 65535
 
-void print_entry(struct dirent *ent) {
-  struct stat filestats;
+typedef struct lsent {
+  char name[1024];
+  blkcnt_t blocks;
+  off_t size;
+} LSENT;
 
-  if (stat(ent->d_name, &filestats)) {
-    printf("%s\n", strerror(errno));
-    exit(1);
-  }
-  printf("%3llu %-9s\n", filestats.st_blocks, ent->d_name);
+void print_entry(LSENT *entry) {
+  printf("%3llu %-9s\n", entry->blocks, entry->name);
 }
 
-void list_contents(DIR *dirp) {
-  // TODO: Sort files like `ls` by default
+int collect_contents(DIR *dirp, LSENT *entries) {
   // TODO: Implement the -a flag; it is on by default now
-  struct dirent *ent;
-
   // TODO: Error checking when calling `readdir`
-  while((ent = readdir(dirp))) {
-    print_entry(ent);
+  struct dirent *direntp;
+
+  LSENT *start = entries;
+  while((direntp = readdir(dirp))) {
+    struct stat filestats;
+
+    if (stat(direntp->d_name, &filestats)) {
+      printf("%s\n", strerror(errno));
+      exit(1);
+    }
+
+    strcpy(entries->name, direntp->d_name);
+    entries->blocks = filestats.st_blocks;
+    entries->size   = filestats.st_size;
+    entries++;
   }
+
+  return entries - start;
 }
 
 int main(int argc, char *argv[]) {
   // TODO: This will have to change when introducing optional flags
   char *path = (argc == 1) ? DEFAULT_DIR : argv[1];
   DIR *dirp = opendir(path);
+  LSENT *entries = malloc(MAX_ENTRIES * sizeof(LSENT));
+  int num_entries;
 
   // TODO: Handle when input is just a file
   if (dirp == NULL) {
@@ -40,8 +55,13 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  list_contents(dirp);
+  // TODO: Sort entries like `ls` by default
+  num_entries = collect_contents(dirp, entries);
+  for (int i = 0; i < num_entries; i++) {
+    print_entry(&entries[i]);
+  }
 
+  free(entries);
   closedir(dirp);
   return 0;
 }
