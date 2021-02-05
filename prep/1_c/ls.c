1ff57d56f6ae37c06cbdcde2ae05df0970d4421e
Rishi Chopra <rqchopra@gmail.com>
2021-02-04 23:01:32 -0600

    Tidy up syntax and style

--- prep/1_c/ls.c
+++ prep/1_c/ls.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 
 #define MAX_DIR_ENTRIES 65535
+#define MAX_PATH_LEN    1024
 
 // Bit masks for ls flags
 #define SHOW_HIDDEN    0b00001
@@ -21,22 +22,52 @@ typedef struct lsent {
   off_t bytes;
 } LSENT;
 
-struct ls_stats {
+typedef struct lsstats {
   blkcnt_t total_blocks;
   off_t total_size;
-};
+} LSSTATS;
 
 // Function prototypes
-void print_summary(struct ls_stats *summary, unsigned char flags);
+unsigned char parse_args(char *path, int argc, char *argv[]);
+int collect_contents(DIR *dirp, LSENT *entries, LSSTATS *summary, unsigned char flags);
+void print_summary(LSSTATS *summary, unsigned char flags);
 void print_entry(LSENT *entry, unsigned char flags);
 int compare_by_size(const void *first, const void *second);
-int collect_contents(DIR *dirp, LSENT *entries, struct ls_stats *summary, unsigned char flags);
 
 int main(int argc, char *argv[]) {
-  unsigned char flags = 0;
-  char *path;
-  char c;
+  char path[MAX_PATH_LEN];
+  unsigned char flags = parse_args(path, argc, argv);
 
+  // TODO: Handle when input is just a file
+  DIR *dirp = opendir(path);
+  if (dirp == NULL) {
+    printf("myls: %s: %s\n", path, strerror(errno));
+    return 1;
+  }
+
+  LSSTATS summary = { 0, 0 };
+  LSENT *entries = malloc(MAX_DIR_ENTRIES * sizeof(LSENT));
+  int num_entries = collect_contents(dirp, entries, &summary, flags);
+  if (flags & SORT_BY_SIZE) {
+    qsort(entries, num_entries, sizeof(LSENT), compare_by_size);
+  }
+
+  print_summary(&summary, flags);
+  for (int i = 0; i < num_entries; i++) {
+    print_entry(&entries[i], flags);
+  }
+  if (!(flags & NEW_LINES)) {
+    printf("\n");
+  }
+
+  free(entries);
+  closedir(dirp);
+  return 0;
+}
+
+unsigned char parse_args(char *path, int argc, char *argv[]) {
+  unsigned char flags = 0;
+  char c;
   // Flag parsing lifted from K&R C, Section 5.10 (pg. 117)
   while (--argc > 0 && (*++argv)[0] == '-') {
     while ((c = *++argv[0])) {
@@ -63,40 +94,12 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+  strcpy(path, argc == 0 ? "." : *argv);
 
-  path = argc == 0 ? "." : *argv;
-
-  DIR *dirp = opendir(path);
-  LSENT *entries = malloc(MAX_DIR_ENTRIES * sizeof(LSENT));
-  int num_entries;
-
-  // TODO: Handle when input is just a file
-  if (dirp == NULL) {
-    printf("myls: %s: %s\n", path, strerror(errno));
-    return 1;
-  }
-
-  struct ls_stats summary = { 0, 0 };
-
-  num_entries = collect_contents(dirp, entries, &summary, flags);
-  if (flags & SORT_BY_SIZE) {
-    qsort(entries, num_entries, sizeof(LSENT), compare_by_size);
-  }
-
-  print_summary(&summary, flags);
-  for (int i = 0; i < num_entries; i++) {
-    print_entry(&entries[i], flags);
-  }
-  if (!(flags & NEW_LINES)) {
-    printf("\n");
-  }
-
-  free(entries);
-  closedir(dirp);
-  return 0;
+  return flags;
 }
 
-int collect_contents(DIR *dirp, LSENT *entries, struct ls_stats *summary, unsigned char flags) {
+int collect_contents(DIR *dirp, LSENT *entries, LSSTATS *summary, unsigned char flags) {
   // TODO: Error checking when calling `readdir`
   struct dirent *direntp;
   int dir_fd = dirfd(dirp);
@@ -125,7 +128,7 @@ int collect_contents(DIR *dirp, LSENT *entries, struct ls_stats *summary, unsign
   return entries - start;
 }
 
-void print_summary(struct ls_stats *summary, unsigned char flags) {
+void print_summary(LSSTATS *summary, unsigned char flags) {
   if (flags & SHOW_SIZE) {
     int size_scale = (flags & SHOW_KILOBYTES) ? 2 : 1;
     printf("total %llu\n", summary->total_blocks / size_scale);
@@ -145,5 +148,7 @@ void print_entry(LSENT *entry, unsigned char flags) {
 int compare_by_size(const void *first, const void *second) {
   off_t fsize = ((LSENT *)first)->bytes;
   off_t ssize = ((LSENT *)second)->bytes;
+  // A tip learned from StackOverflow, this avoids problems with
+  // overflow from doing something like: return ssize - fsize;
   return (fsize < ssize) - (fsize > ssize);
 }
