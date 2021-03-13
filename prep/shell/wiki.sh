f435d56e4a6b1615e06787eabffa9364afbbf270
Rishi Chopra <rqchopra@gmail.com>
2021-03-13 12:51:39 -0600

    Fetch the first sentence of wikipedia intros
    This is the bare minimum functionality for the first portion of this
    exercise. The MediaWiki API seems especially suited for this purpose, as
    one can specifically request only the first sentence.
    
    There is no error handling here, nor is the functionality regarding
    section headers implmented yet.

--- prep/shell/wiki.sh
+++ prep/shell/wiki.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo 'wikipedia'
+# Grabbing JSON from the [MediaWiki TextExtracts
+# API](https://www.mediawiki.org/wiki/Extension:TextExtracts#API)
+WIKI_BASE="https://en.wikipedia.org/w/api.php"
+PARAMS="action=query&format=json&prop=extracts&explaintext=1&exsentences=1"
+
+#TODO: Error handling from curl
+#TODO: What happens if a page is not found
+#TODO: Handle multi-word/punctuation inputs
+RESP=$(curl -s -XGET "$WIKI_BASE?$PARAMS&titles=$1")
+
+# I really want to use [jq](https://stedolan.github.io/jq/) for this but it
+# seems counter to the spirit of this exercise, so here's some gnarly sed
+# instead.
+INTRO=$(echo "$RESP" | sed -n 's/^.*extract":"\(.*\)".*$/\1/p')
+
+echo "$INTRO"
