175c8f3783cf1ad4dd2d191ff1baf533d2777981
Rishi Chopra <rqchopra@gmail.com>
2021-03-16 09:01:52 -0500

    Add completion with top search result
    This is for the main search term only and will grab the top search
    result from Wikipedia's search API upon tab completion.
    
    Still have to handle the section completion for the second argument.

--- prep/shell/wiki-completion.bash
+++ prep/shell/wiki-completion.bash
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-complete -W "walrus chess earth" wiki
+WIKI_BASE="https://en.wikipedia.org/w/api.php"
+PARAMS="action=opensearch&format=json&formatversion=2&namespace=0&limit=1"
+
+search_wikipedia() {
+  curl -s -XGET "$WIKI_BASE?$PARAMS&search=${COMP_WORDS[1]}" |
+    cut -d, -f2 |
+    sed -n 's/\[\(.*\)\]/\1/p' |
+    sed 's/ /_/g' |
+    sed 's/,/ /g'
+}
+
+page_completions() {
+  OPTIONS=$(search_wikipedia)
+  COMPREPLY=($(compgen -W "$OPTIONS" -- "${COMP_WORDS[1]}"))
+}
+
+complete -F page_completions wiki
