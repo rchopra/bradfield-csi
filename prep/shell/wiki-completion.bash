a936835f8539d4cdaf41d250d450e68018db3460
Rishi Chopra <rqchopra@gmail.com>
2021-03-20 12:58:33 -0500

    Add explanatory comments to completions

--- prep/shell/wiki-completion.bash
+++ prep/shell/wiki-completion.bash
@@ -5,15 +5,19 @@ PARAMS="action=opensearch&format=json&formatversion=2&namespace=0&limit=1"
 
 search_wikipedia() {
   curl -s -XGET "$WIKI_BASE?$PARAMS&search=${COMP_WORDS[1]}" |
-    cut -d, -f2 |
-    sed -n 's/\[\(.*\)\]/\1/p' |
-    sed 's/ /_/g' |
-    sed 's/,/ /g'
+    cut -d, -f2 |                # Cut out the results array, will give "[...]"
+    sed -n 's/\[\(.*\)\]/\1/p' | # Remove square brackets
+    sed 's/ /_/g' |              # Replace spaces with underscores
+    sed 's/,/ /g'                # Replace commas with spaces
 }
 
 page_completions() {
   OPTIONS=$(search_wikipedia)
   COMPREPLY=($(compgen -W "$OPTIONS" -- "${COMP_WORDS[1]}"))
+
+  # For section completion, can grab sections from the parse API
+  # Need to enable only when the second argument is present, using the first
+  # argument as an input
 }
 
 complete -F page_completions wiki
